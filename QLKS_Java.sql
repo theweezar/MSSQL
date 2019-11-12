50c74db94a0f575d85dd1d98a0f0207a1af3f66c
Duc <minhducducminh1999@gmail.com>
2019-11-12 23:26:24 +0700

    committed

--- QLKS_Java.sql
+++ QLKS_Java.sql
@@ -7,6 +7,12 @@ create table BOPHAN(
 	tenBP varchar(50) NOT NULL UNIQUE
 )
 
+create table DICHVU(
+	maDV varchar(50) PRIMARY KEY,
+	tenDV text NOT NULL,
+	gia int NOT NULL
+)
+
 create table NHANVIEN(
 	username varchar(100) PRIMARY KEY,
 	pw varchar(100) NOT NULL,
@@ -66,14 +72,33 @@ create table CTPD(
 create table PHIEUTHUE(
 	maPT varchar(50) PRIMARY KEY,
 	cmnd varchar(20) NOT NULL REFERENCES KHACH(cmnd),
-	maNV varchar(30) NOT NULL REFERENCES NHANVIEN(maNV),
+	username varchar(100) NOT NULL REFERENCES NHANVIEN(username),
 	ngayden date NOT NULL,
-	gioden time
 )
 
 create table CTPT(
 	maPT varchar(50) REFERENCES PHIEUTHUE(maPT),
 	maphong varchar(10) REFERENCES PHONG(maphong),
-	maKM varchar(50) REFERENCES KHUYENMAI(maKM),
 	PRIMARY KEY(maPT,maphong)
+)
+
+create table CTPDV(
+	maPT varchar(50) REFERENCES PHIEUTHUE(maPT),
+	maphong varchar(10) REFERENCES PHONG(maphong),
+	maDV varchar(50) NOT NULL REFERENCES DICHVU(maDV),
+	ngaydat date NOT NULL,
+	giodat time NOT NULL,
+	soluong int NOT NULL
+)
+
+create table HOADON(
+	maHD varchar(50) PRIMARY KEY,
+	maPT varchar(50) NOT NULL REFERENCES PHIEUTHUE(maPT),
+	maNV varchar(100) NOT NULL REFERENCES NHANVIEN(username),
+	ngaylap date NOT NULL,
+	giolap time NOT NULL,
+	tienphong int NOT NULL,
+	tiendv int NOT NULL,
+	tiennhan int NOT NULL,
+	tienthua int NOT NULL,
 )
