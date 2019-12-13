edbbd539acf78edd950ebc43d9ff1a91041ce66c
minhduc <minhducducminh1999@gmail.com>
2019-12-13 19:53:02 +0700

    committed

---
+++ Query/QLSV_ThayThu.sql
@@ -0,0 +1,20 @@
+﻿create database QLSV
+
+use QLSV
+
+/*
+Xác định thực thể:
+- Giảng viên
+- Khoa 
+- Chuyên ngành 
+- Lớp
+- Bảng kế hoạch giảng
+- Sinh viên
+- Lớp tín chỉ
+- Môn học
+- Bảng kết quả
+*/
+
+/*
+
+*/
